074318d0db0a1f9bfb8a969aa84c308df0ee3e35
Tiffany Nguyen <trucn0311@gmail.com>
2022-11-16 23:04:24 -0600

    complete final recipe app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'da6e0a16-efc8-422e-84cf-814f1011e0b4';
+export const MODAL_CLOSE_SEC = 2.5;
